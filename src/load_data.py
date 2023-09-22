25188f1f593a9f3a158ffb389a31d3fda7fb4b2f
Raffi Hotter <raphael.hotter@gmail.com>
2023-09-22 12:50:59 -0400

    Add load_retinotopy function for loading all the data

---
+++ src/load_data.py
@@ -0,0 +1,59 @@
+import numpy as np
+import mne
+import os
+import pandas as pd
+
+def _get_annotations(annot_file):
+    """
+    Load annotations from a .tsv file.
+
+    Parameters
+    ----------
+    annot_file : str
+        Path to .tsv file.
+
+    Returns
+    -------
+    annots : mne.Annotations
+        Annotations object.
+    """
+
+    annots_df = pd.read_csv(annot_file, sep="\t")
+    onsets = annots_df.Onset.tolist()
+    durations = list(np.array(onsets[1:]) - np.array(onsets[:-1])) + [0]
+
+    annots = mne.Annotations(
+        onset=onsets,  # in seconds
+        duration=durations,  # in seconds, too
+        description=annots_df.trial_type.tolist(),
+    )
+
+    return annots
+
+def load_retinotopy_data():
+    """
+    Load steve's retinotopy data. He looks at left/right checkerboard stimuli.
+
+    Returns
+    -------
+    data : list of mne.io.Raw
+        Intensity data.
+    """
+
+    # load all .snirf files in data/Study1/Steve.
+    data = []
+
+    for file in os.listdir("data/Study1/Steve"):
+        if file.endswith(".snirf"):
+            snirf_file = os.path.join("data/Study1/Steve", file)
+            annots_file = snirf_file.replace(".snirf", "_events.tsv")
+
+            nirs = mne.io.read_raw_snirf(snirf_file)
+            annots = _get_annotations(annots_file)
+            nirs.set_annotations(annots)
+
+            data.append(nirs)
+
+    return data
+            
+            
